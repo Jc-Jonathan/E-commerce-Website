2339de761b018b3d38f374cdaff94379c9a244fc
Jonathan Chaufa <chaufajonathan459@gmail.com>
2025-09-01 16:09:23 +0530

    edit

--- FRONTENT/frontent/src/Components/Item/Item.tsx
+++ FRONTENT/frontent/src/Components/Item/Item.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Item.css";
 import { Link } from "react-router-dom";
+import { FaHeart } from "react-icons/fa"; // ❤️ heart icon
 
 interface ItemProps {
   id: number | string;
@@ -11,9 +12,21 @@ interface ItemProps {
 }
 
 const Item: React.FC<ItemProps> = ({ id, name, image, new_price, old_price }) => {
+  const [liked, setLiked] = useState(false);
+
+  const toggleLike = () => {
+    setLiked(!liked);
+  };
+
   return (
     <div className="item">
-     <Link to={`/product/${id}`}><img src={image} alt={name} className="item-image" /></Link> 
+      <div className="item-like" onClick={toggleLike}>
+        <FaHeart className={`heart-icon ${liked ? "liked" : ""}`} />
+      </div>
+
+      <Link to={`/product/${id}`}>
+        <img src={image} alt={name} className="item-image" />
+      </Link>
       <h3 className="item-name">{name}</h3>
       <div className="item-prices">
         <span className="old-price">₹{old_price}</span>
